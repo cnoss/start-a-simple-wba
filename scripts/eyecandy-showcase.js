6a152448464f6648675616644bc05d721cecfb31
cnoss <christian.noss@fh-koeln.de>
2017-11-22 23:37:21 +0100

    Javascript Beispiele eingebaut.

--- scripts/eyecandy-showcase.js
+++ scripts/eyecandy-showcase.js
@@ -6,13 +6,31 @@ Färbt die Showcase Items hübsch ein
 
 var eyecandyShowcase = {};
 eyecandyShowcase.basecolor = [147, 19, 206];
+eyecandyShowcase.colors = [
+    [147, 19, 206],
+    [221, 17, 102],
+    [73, 82, 225]
+];
 eyecandyShowcase.lastAdd = 0;
-eyecandyShowcase.getColor = function() {
 
-    var a = Math.random() + 0.5;
-    while (Math.abs(eyecandyShowcase.lastAdd - a) < 0.1) { a = Math.random() + 0.4; }
+eyecandyShowcase.getOpacity = function() {
+    return (Math.floor(Math.random() * 4) + 6) / 10;
+}
+
+eyecandyShowcase.getColor = function(mode) {
+    if (mode === "single") {
+        return eyecandyShowcase.basecolor;
+    } else {
+        var rand = Math.floor(Math.random() * eyecandyShowcase.colors.length);
+        return eyecandyShowcase.colors[rand];
+    }
+}
+
+eyecandyShowcase.getRGBA = function() {
+    var a = eyecandyShowcase.getOpacity();
+    while (Math.abs(eyecandyShowcase.lastAdd - a) < 0.1) { a = eyecandyShowcase.getOpacity(); }
     eyecandyShowcase.lastAdd = a;
-    var color = eyecandyShowcase.basecolor;
+    var color = eyecandyShowcase.getColor();
     return color.join(",") + "," + a;
 }
 
@@ -24,9 +42,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // Über die Elemente interieren …
     for (i = 0; i < showcase__items.length; i++) {
-        var rgba = eyecandyShowcase.getColor();
+        var rgba = eyecandyShowcase.getRGBA();
         showcase__items[i].style.backgroundColor = "rgba(" + rgba + ")";
     }
-
-
 });
